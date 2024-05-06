7fc99e42d51e2ad949197599afc4ed949ab42397
DjojovdEerden <144007760+DjojovdEerden@users.noreply.github.com>
2024-05-06 11:45:23 +0200

    1
    1

---
+++ assets/php/register.php
@@ -0,0 +1,40 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    // Form validation: Ensure that the form is correctly filled
+    if (empty($username)) { array_push($errors, "Username is required"); }
+    if (empty($email)) { array_push($errors, "Email is required"); }
+    if (empty($password)) { array_push($errors, "Password is required"); }
+
+    // connect to the database
+    $con = new mysqli("localhost", "root", "", "login_sample_db");
+    if ($con->connect_error) {
+        die("Connection failed: " . $con->connect_error);
+    } else {
+        // Check if the username or email already exists
+        $stmt = $con->prepare("SELECT * FROM users WHERE user_name = ? OR email = ?");
+        $stmt->bind_param("ss", $username, $email);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        if ($result->num_rows > 0) {
+            array_push($errors, "Username or email already exists");
+        } else {
+            // Hash the password
+            $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+
+            $stmt = $con->prepare("INSERT INTO users (email, user_name, password, date) VALUES (?, ?, ?, NOW())");
+            $stmt->bind_param("sss", $email, $username, $hashed_password);
+            if ($stmt->execute()) {
+                // Redirect to login.php
+                header('Location: /html/login.php');
+                exit;
+            } else {
+                echo "Error: " . $stmt->error;
+            }
+        }
+    }
+}
+?>
